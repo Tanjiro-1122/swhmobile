0cd729f81899f802639fab14023296a27e4b0b4d
J Huertas <huertasfam@gmail.com>
2025-11-25 08:03:23 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -239,7 +239,7 @@ export default function Layout({ children, currentPageName }) {
       </div>
 
       {/* Main Content */}
-        <main className="pt-20 min-h-screen">
+        <main className="flex-1">
           <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 max-w-7xl">
             {children}
           </div>
