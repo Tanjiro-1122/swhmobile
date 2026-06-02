0c8041021a253b851dfb5f91dd278d1389b0b8e1
Tanjiro-1122 <huertasfam1@icloud.com>
2026-06-02 19:48:49 -0400

    feat: Add Sentry initialization for error monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 import '@/globals.css'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", "sportswagerhelper.com", /^\//],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <App />
   // </React.StrictMode>,
-) 
+)
